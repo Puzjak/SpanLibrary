16ec388ae3eb0fd2f004509c41a388b9a1fb45d9
Matija Puzjak <mpuzjak@span.eu>
2022-07-24 16:29:58 +0200

    Implement book paging, filtering and sorting

--- src/SpanAcademy.SpanLibrary.Application/Books/IBookService.cs
+++ src/SpanAcademy.SpanLibrary.Application/Books/IBookService.cs
@@ -1,10 +1,11 @@
 ﻿using SpanAcademy.SpanLibrary.Application.Books.Models;
+using SpanAcademy.SpanLibrary.Application.Collections;
 
 namespace SpanAcademy.SpanLibrary.Application.Books
 {
     public interface IBookService
     {
-        public Task<IReadOnlyList<BookDto>> GetBooks(bool onlyActive, CancellationToken token);
+        public Task<IPagedList<BookDto>> GetBooks(GetBooksDto getBooksDto, CancellationToken token);
         public Task<BookDto> GetById(int id, CancellationToken token);
         public Task<int> CreateBook(CreateBookDto book, CancellationToken token);
         public Task UpdateBook(UpdateBookDto book, CancellationToken token);
