9e189e653ef8abf9df613466aaaa5975edc93067
Matija Puzjak <mpuzjak@span.eu>
2022-07-24 16:48:34 +0200

    Implement Author and Publisher API-s

--- src/SpanAcademy.SpanLibrary.Application/Publishers/IPublisherService.cs
+++ src/SpanAcademy.SpanLibrary.Application/Publishers/IPublisherService.cs
@@ -1,7 +1,14 @@
-﻿namespace SpanAcademy.SpanLibrary.Application.Publishers
+﻿using SpanAcademy.SpanLibrary.Application.Publishers.Models;
+
+namespace SpanAcademy.SpanLibrary.Application.Publishers
 {
     public interface IPublisherService
     {
-        Task<bool> PublisherExists(int id, CancellationToken token);
+        public Task<IReadOnlyList<PublisherDto>> GetPublishers(CancellationToken token);
+        public Task<PublisherDto> GetById(int id, CancellationToken token);
+        public Task<int> CreatePublisher(CreatePublisherDto publisher, CancellationToken token);
+        public Task UpdatePublisher(UpdatePublisherDto publisher, CancellationToken token);
+        public Task<bool> DeletePublisher(int id, CancellationToken token);
+        public Task<bool> PublisherExists(int id, CancellationToken token);
     }
 }
